c2d5ae10cc2717a47944e0ff0de116652eba29d5
Никита Кулагин <nikitakulagin@MacBook-Pro-Nikita.local>
2025-02-07 18:16:15 +0300

    Устраняем проблему с файлами

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './Home';
 import Login from './Login';
@@ -39,7 +39,7 @@ function App() {
 
 
   return (
-    <Router basename="/SCB-Research-Portal">
+    <Router>
       <div className="App">
       <Routes>
         <Route path="/" element={<Home isAuthenticated={isAuthenticated} />} />
