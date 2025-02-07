83cc86334ff2ef164517a1a07282bab154d231d3
Никита Кулагин <nikitakulagin@MacBook-Pro-Nikita.local>
2025-02-07 18:36:04 +0300

    Устраняем проблему с файлами

--- src/initData.js
+++ src/initData.js
@@ -1,24 +1,24 @@
 // initData.js
 
 const initialResearchData = [
-    {
-        id: 1,
-        title: 'Макромонитор 2025.02.03',
-        domain: 'Макромонитор',
-        periodicity: 'Еженедельное',
-        date: '2025-02-03',
-        pdfUrl: `${process.env.PUBLIC_URL}/pdfs/Макромонитор 2025.02.03.pdf`,
-        thumbnailUrl: `${process.env.PUBLIC_URL}/thumbnails/Макромонитор 2025.02.03.png`,
-      },
-      {
-        id: 2,
-        title: 'Рынок пшеницы Февраль 2025',
-        domain: 'Зерновые',
-        periodicity: 'Ежеквартальное',
-        date: '2025-02-05',
-        pdfUrl: `${process.env.PUBLIC_URL}/pdfs/Рынок пшеницы 2025.02.01.pdf`,
-        thumbnailUrl: `${process.env.PUBLIC_URL}thumbnails/Рынок пшеницы 2025.02.01.png`,
-      },
-  ];
-  
-  export default initialResearchData;
+  {
+    id: 1,
+    title: 'Макромонитор 2025.02.03',
+    domain: 'Макромонитор',
+    periodicity: 'Еженедельное',
+    date: '2025-02-03',
+    pdfUrl: `${process.env.PUBLIC_URL}/pdfs/Макромонитор 2025.02.03.pdf`,
+    thumbnailUrl: `${process.env.PUBLIC_URL}/thumbnails/Макромонитор 2025.02.03.png`,
+  },
+  {
+    id: 2,
+    title: 'Рынок пшеницы Февраль 2025',
+    domain: 'Зерновые',
+    periodicity: 'Ежеквартальное',
+    date: '2025-02-05',
+    pdfUrl: `${process.env.PUBLIC_URL}/pdfs/Рынок пшеницы 2025.02.01.pdf`,
+    thumbnailUrl: `${process.env.PUBLIC_URL}/thumbnails/Рынок пшеницы 2025.02.01.png`,
+  },
+];
+
+export default initialResearchData;
